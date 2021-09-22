94e937544df9fcbc6b25687a1cb044ea13361c44
Marius <Mnv22081@gmail.com>
2021-09-22 21:47:30 +0300

    created about us

--- src/app/pages/home-page/home-page.module.ts
+++ src/app/pages/home-page/home-page.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page.component';
 import { SharedModule } from '../../shared/shared.module';
+import { AboutUsComponent } from './components/about-us/about-us.component';
 
 const routes: Routes = [{ path: '', component: HomePageComponent }];
 
 @NgModule({
-  declarations: [HomePageComponent],
+  declarations: [HomePageComponent, AboutUsComponent],
   imports: [SharedModule, RouterModule.forChild(routes), CommonModule],
 })
 export class HomePageModule {}
