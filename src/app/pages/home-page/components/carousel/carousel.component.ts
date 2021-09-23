294e8babbf7508cbfbe6b0d65fe605e7c7aeba75
Alexandru Lolea <alexandru.lolea@ligaac.ro>
2021-09-23 22:30:14 +0300

    Carousel done

--- src/app/pages/home-page/components/carousel/carousel.component.ts
+++ src/app/pages/home-page/components/carousel/carousel.component.ts
@@ -9,13 +9,33 @@ import { Articles } from '../../../../models/articles';
   animations: [
     trigger('carouselAnimation', [
       transition('void => *', [style({ opacity: 0 }), animate('300ms', style({ opacity: 1 }))]),
-      transition('* => void', [animate('300ms', style({ opacity: 0 }))]),
+      // transition('* => void', [animate('300ms', style({ opacity: 0 }))]),
     ]),
   ],
 })
 export class CarouselComponent {
   number = 0;
   articles: Articles[] = [
+    {
+      image: './assets/images/Frame 2.svg',
+      link: '/about-us',
+    },
+    {
+      image: './assets/images/photo 02.svg',
+      link: '/about-us',
+    },
+    {
+      image: './assets/images/photo 02.svg',
+      link: '/about-us',
+    },
+    {
+      image: './assets/images/Frame 2.svg',
+      link: '/about-us',
+    },
+    {
+      image: './assets/images/photo 02.svg',
+      link: '/about-us',
+    },
     {
       image: './assets/images/Frame 2.svg',
       link: '/about-us',
