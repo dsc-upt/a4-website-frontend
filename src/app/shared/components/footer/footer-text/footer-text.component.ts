018963663ad27bf07b20e407202fbc13fd926b6f
Leonardo <leotoma4@gmail.com>
2021-09-23 17:29:44 +0300

    Make component for made with love

---
+++ src/app/shared/components/footer/footer-text/footer-text.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-text',
+  templateUrl: './footer-text.component.html',
+  styleUrls: ['./footer-text.component.html.scss'],
+})
+export class footerTextComponent {
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FooterComponent } from './components/footer/footer.component';
 import { MenuComponent } from './components/menu/menu.component';
+import {footerTextComponent} from "./components/footer/footer-text/footer-text.component";
 
 @NgModule({
-  declarations: [FooterComponent, MenuComponent],
-  exports: [MenuComponent, FooterComponent],
+    declarations: [FooterComponent, MenuComponent, footerTextComponent],
+  exports: [MenuComponent, FooterComponent, footerTextComponent],
   imports: [CommonModule, RouterModule],
 })
 export class SharedModule {}
