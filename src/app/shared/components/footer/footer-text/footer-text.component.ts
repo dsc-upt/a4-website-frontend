965aab26bf017eae493a1977d0ff9d562e72afbb
Leonardo <leotoma4@gmail.com>
2021-09-24 12:55:59 +0300

    update made with love

--- src/app/shared/components/footer/footer-text/footer-text.component.ts
+++ src/app/shared/components/footer/footer-text/footer-text.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer-text',
   templateUrl: './footer-text.component.html',
-  styleUrls: ['./footer-text.component.html.scss'],
+  styleUrls: ['./footer-text.component.scss'],
 })
 export class footerTextComponent {
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,11 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FooterComponent } from './components/footer/footer.component';
 import { MenuComponent } from './components/menu/menu.component';
-import {footerTextComponent} from "./components/footer/footer-text/footer-text.component";
+import { footerTextComponent } from './components/footer/footer-text/footer-text.component';
 
 @NgModule({
-    declarations: [FooterComponent, MenuComponent, footerTextComponent],
-  exports: [MenuComponent, FooterComponent, footerTextComponent],
+  declarations: [FooterComponent, MenuComponent, footerTextComponent],
+  exports: [MenuComponent, FooterComponent,footerTextComponent],
   imports: [CommonModule, RouterModule],
 })
 export class SharedModule {}
